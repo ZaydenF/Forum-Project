b904cb85ab963207519e298c399888b6c00ce52f
ZaydenF <zaydenfleming@gmail.com>
2025-01-07 12:53:24 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,7 +15,7 @@ from pymongo import DESCENDING
 app = Flask(__name__)
 
 app.debug = False #Change this to False for production
-#os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #Remove once done debugging
+
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
@@ -78,7 +78,7 @@ def get_posts():
 #redirect to GitHub's OAuth page and confirm callback URL
 @app.route('/login')
 def login():   
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 @app.route('/logout')
 def logout():
